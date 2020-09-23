130381fd3554094e8d293b1b9ffec955260e3c8e
Manish Kumar <mk778281@gmail.com>
2020-09-23 16:02:53 +0530

    build

---
+++ client/build/precache-manifest.3fba33ebbf6773b691a5f0982052445a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9d09bffa2d99338d5a4a2c06e7a78a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9eccff9c7128d498446",
+    "url": "/static/css/main.206e655f.chunk.css"
+  },
+  {
+    "revision": "28d25995902fd405db11",
+    "url": "/static/js/2.5143860a.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.5143860a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9eccff9c7128d498446",
+    "url": "/static/js/main.d2315093.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  }
+]);
